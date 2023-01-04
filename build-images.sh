16b4a76a256bdc0e8529d0756d34c74ef509ff83
yu.deng <dengyu326@gmail.com>
2023-01-04 12:06:14 +0800

    feishubot yamls

--- build-images.sh
+++ build-images.sh
@@ -1,26 +1,32 @@
 #!/bin/bash
 
 IMAGE_REPO=${IMAGE_REPO:-kubegems}
+[[ ! -z $PUSH_IMAGE ]] && echo "env PUSH_IMAGE not set, not push image"
 
+
+echo "========================================================================"
 echo "build xvfb base image ..."
 pushd ppxvfb-dockerfile
     docker build -t ppxvfb:v1 .
 popd
 
+echo "========================================================================"
 echo "build chatgpt-api image ..."
 
 docker build -t ${IMAGE_REPO}/chatgpt-api:latest .
-[[ ! -z $NO_PUSH ]] && docker push ${IMAGE_REPO}/chatgpt-api:latest
+[[ ! -z $PUSH_IMAGE ]] && docker push ${IMAGE_REPO}/chatgpt-api:latest
 
+echo "========================================================================"
 echo "build proxy image ..."
 pushd proxy
     docker build -t ${IMAGE_REPO}/chatgpt-api-proxy:latest .
-    [[ ! -z $NO_PUSH ]] && docker push ${IMAGE_REPO}/chatgpt-api-proxy:latest
+    [[ ! -z $PUSH_IMAGE ]] && docker push ${IMAGE_REPO}/chatgpt-api-proxy:latest
 popd
 
+echo "========================================================================"
 echo "build feishubot image ..."
 pushd feishubot
     docker build -t ${IMAGE_REPO}/chatgpt-api-feishubot:latest .
-    [[ ! -z $NO_PUSH ]] && docker push ${IMAGE_REPO}/chatgpt-api-feishubot:latest
+    [[ ! -z $PUSH_IMAGE ]] && docker push ${IMAGE_REPO}/chatgpt-api-feishubot:latest
 popd
 
